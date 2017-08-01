1b8811c79d560e25d6a33ebd3a1c56c77697fa83
LeoXugu <robinleoknauth@gmail.com>
2017-08-01 14:13:53 -0700

    added SELECT_name solutions and editing

--- 5_SUM_and_COUNT_solutions.sql
+++ 5_SUM_and_COUNT_solutions.sql
@@ -43,3 +43,34 @@ What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 SELECT SUM(population)
 FROM world
 WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
+
+
+
+/*
+6)
+For each continent show the continent and number of countries.
+*/
+
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
+
+/*
+7)
+For each continent show the continent and number of countries with populations of at least 10 million.
+*/
+
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000
+GROUP BY continent;
+
+/*
+8)
+List the continents that have a total population of at least 100 million.
+*/
+
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000 ;

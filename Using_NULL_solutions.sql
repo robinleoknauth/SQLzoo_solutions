832d35271dba684a55bf0c02d057c081547b5fbf
LeoXugu <robinleoknauth@gmail.com>
2017-08-01 11:47:27 -0700

    Self_Join solutions

--- Using_NULL_solutions.sql
+++ Using_NULL_solutions.sql
@@ -82,30 +82,12 @@ LEFT JOIN dept ON  (teacher.dept = dept.id);
 
 /*
 10)
-
+Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
 */
 
-/*
-11)
-
-*/
-
-/*
-12)
-
-*/
-
-/*
-13)
-
-*/
-
-/*
-14)
-
-*/
-
-/*
-15)
-
-*/
+SELECT teacher.name,
+CASE WHEN teacher.dept IN (1,2) THEN 'Sci'
+WHEN teacher.dept = 3 THEN 'Art'
+ELSE 'None' END
+FROM teacher
+LEFT JOIN dept ON  (teacher.dept = dept.id);

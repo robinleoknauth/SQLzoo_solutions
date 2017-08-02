976095d9bce4fae6d023fa95c797d846d3a0ef1f
LeoXugu <robinleoknauth@gmail.com>
2017-08-02 12:45:17 -0700

    music tutorial

--- music_tutorial_solutions.sql
+++ music_tutorial_solutions.sql
@@ -54,60 +54,55 @@ WHERE song LIKE '%heart%'
 GROUP BY 1
 
 /*
-)
-
+6)
+A "title track" is where the song is the same as the title. Find the title tracks.
 */
 
+SELECT song
+FROM album
+JOIN track ON (asin = album)
+WHERE song = title;
 
 /*
-)
-
+7)
+An "eponymous" album is one where the title is the same as the artist (for example the album 'Blur' by the band 'Blur'). Show the eponymous albums.
 */
 
+SELECT title
+FROM album
+WHERE artist = title;
 
 /*
-)
-
+8)
+Find the songs that appear on more than 2 albums. Include a count of the number of times each shows up.
 */
 
+SELECT track.song, COUNT(album.title) AS Song_count
+FROM album JOIN track ON (album.asin = track.album)
+GROUP BY track.song
+HAVING COUNT(DISTINCT album.title) > 2
 
 /*
-)
-
+9)
+A "good value" album is one where the price per track is less than 50 pence. Find the good value album - show the title, the price and the number of tracks.
 */
 
+SELECT title, price, COUNT(song)
+FROM album JOIN track ON (album.asin = track.album)
+GROUP BY 1, 2
+HAVING price / COUNT(song) < 0.5
 
 /*
-)
+10)
+Wagner's Ring cycle has an imposing 173 tracks, Bing Crosby clocks up 101 tracks.
 
+List albums so that the album with the most tracks is first. Show the title and the number of tracks
+
+-- NOTE this will come back as incorrect, "too many rows",
+but it returns exactly what their example wants you to return.
 */
 
-
-/*
-)
-
-*/
-
-
-/*
-)
-
-*/
-
-
-/*
-)
-
-*/
-
-
-/*
-)
-
-*/
-
-
-/*
-)
-
-*/
+SELECT title, COUNT(asin)
+FROM album JOIN track ON (album.asin = track.album)
+GROUP BY 1
+ORDER BY 2 DESC
